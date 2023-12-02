8a296da95a4d0f3f0ff8d1bb3cf22d8c7d20fbfd
Charles Dudley <charles.duds@gmail.com>
2023-12-01 22:40:28 -0600

    figured out some GUI features with a mockup

--- project/client/for_threads.py
+++ project/client/for_threads.py
@@ -1,3 +1,14 @@
+"""
+	This file contains a sketch for a GUI that can display changing text.
+
+	Current idea is to have one box that updates with all responses from Cybot server
+		and another text entry window that allows user to send requests.
+
+	I have spent 0 time trying to make this look good; just focusing on functionality.
+
+	Should be able to link up to client program and test on Saturday!
+
+"""
 import tkinter as tk
 from tkinter import *
 
@@ -5,40 +16,47 @@ from tkinter import *
 seed = 0
 
 
-def increment():
+def inc():
+	global seed
 	seed += 1
 
 
-root = Tk()
+class MainWindow:
+	def __init__(self, master):
+		self.master = master
+		self.inc_btn = tk.Button(text='Manual Control',
+                                 width=30,
+                                 height=30,
+                                 command=self.update_count)
+		self.count = StringVar()
+		self.count.set(hash(seed))
 
-L1 = Label(root, text="Responses")
-L1.grid(row=0,column=0)
-L2 = Label(root, text="Requests")
-L2.grid(row=1,column=0)
+		self.inc_btn.grid(row=2,column=0)
+		self.L1 = Label(master, text="Responses")
+		self.L1.grid(row=0,column=0)
+		self.L2 = Label(master, text="Requests")
+		self.L2.grid(row=1,column=0)
 
-response = StringVar()
-response.set('example')
+		self.entry = Entry(textvariable=self.count,
+					  state=DISABLED).grid(row=0,
+										   column=1,
+										   padx=10,
+										   pady=10)
+		self.request = Entry().grid(row=1,column=1,
+							   padx=10,pady=10)
+	def update_count(self):
+		inc()
+		self.count.set(hash(seed))
 
-entry = Entry(textvariable=response,
-			  state=DISABLED).grid(row=0,
-			  					   column=1,
-								   padx=10,
-								   pady=10)
 
-request = Entry().grid(row=1,column=1,
-					   padx=10,pady=10)
 
-# responses_box = Text(root,height=10,width=50)
-# response_lbl = Label(root,text="responses")
-# 
-# response = hash(seed)
-# 
-increase_btn = Button(root,text="increase",command=increment)
-# 
-# response_lbl.pack()
-# responses_box.pack()
-# increase_btn.pack()
-# 
-# responses_box.insert(tk.END, response)
+def main():
+	root = tk.Tk()
+	# root.geometry("600x350")
+	main = MainWindow(root)
+	root.mainloop()
 
-mainloop()
+
+if __name__ == '__main__':
+	main()
+
